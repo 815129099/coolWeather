8bd960f03952b5287d5a817e35a576432e29668c
lwx <815129099@qq.com>
2018-08-29 14:27:08 +0800

    添加实体类

---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/AQI.java
@@ -0,0 +1,10 @@
+package android.coolweather.com.coolweather.gson;
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
